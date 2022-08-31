5c5a050bda9224b55ad371d7b35e4ed3b334a621
epmt6528 <epmt6528@gmail.com>
2022-08-30 20:18:19 -0700

    Added index files for components

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,1 +1,3 @@
 export {default as WeatherCardLarge} from './WeatherCardLarge/WeatherCardLarge'
+export {default as WeatherCardSmall} from './WeatherCardSmall/WeatherCardSmall'
+export {default as WeatherList} from './WeatherList/WeatherList'
