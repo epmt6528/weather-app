b881db7522b61cb038ff75be879d61abdb95f6be
epmt6528 <epmt6528@gmail.com>
2022-08-30 20:15:48 -0700

    Created custom hook to fetch API data

--- src/services/api/useOpenWeatherMap.ts
+++ src/services/api/useOpenWeatherMap.ts
@@ -1,39 +1,34 @@
-import { fetcher } from '../fetcher/fetcher'
 import useSWR from 'swr'
+import { fetcher } from '../fetcher/fetcher'
 import { base_url } from '../../consts/apiUrl'
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { SelectedCityContext } from '../../context/SelectedCityContextWrapper'
 import { convertUtcTimeToLocal, getTomorrowDate } from '../formatter/dateAndTime'
 
 const useOpenWeatherMap = () => {
-  // @ts-ignore
   const { selectedCityContext } = useContext(SelectedCityContext)
   const [selectedCity,] = selectedCityContext
   const API_KEY = process.env.REACT_APP_OPEN_WEATHER_API_KEY
   const { data, error } = useSWR(`${base_url}?id=${selectedCity.id}&APPID=${API_KEY}`, fetcher)
-  // const [fiveDaysData, setFiveDaysData] = useState([])
+  const [fiveDaysData, setFiveDaysData] = useState<any[]>([])
   const tomorrowDate = getTomorrowDate()
-  console.log()
 
   useEffect(()=>{
     if(data){
       const weatherDataList = data.list
-      let fiveDaysDataTemp = []
-      fiveDaysDataTemp.push(weatherDataList[0].dt_txt)
-
+      let fiveDaysDataTemp: any[] = []
+      fiveDaysDataTemp.push(weatherDataList[0])
       const tomorrowDataIndex = weatherDataList.findIndex((weatherData: any) => convertUtcTimeToLocal(weatherData.dt_txt) === tomorrowDate)
-      console.log(tomorrowDataIndex)
+
+      for(let i = 1; i < 5; i++){
+        fiveDaysDataTemp.push(weatherDataList[tomorrowDataIndex + i*7])
+      }
+
+      setFiveDaysData(fiveDaysDataTemp)
     }
   }, [data])
 
-  // console.log(data&&data.list&&data.list[0].dt_txt)
-  // console.log(data&&data.list&&data.list[7].dt_txt)
-  // console.log(data&&data.list&&data.list[15].dt_txt)
-  // console.log(data&&data.list&&data.list[23].dt_txt)
-  // console.log(data&&data.list&&data.list[31].dt_txt)
-  // console.log(data&&data.list&&data.list[39].dt_txt)
-
-  return { data, error }
+  return { data, error, fiveDaysData }
 }
 
 export default useOpenWeatherMap
--- src/services/formatter/dateAndTime.ts
+++ src/services/formatter/dateAndTime.ts
@@ -1,12 +1,14 @@
 import dayjs from 'dayjs';
 
 import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+import { dayArray } from '../../consts/texts/english'
 dayjs.extend(timezone);
+dayjs.extend(utc);
 
 export const convertUtcTimeToLocal = (utcTime: string) => {
   const localTimeZone = dayjs.tz.guess();
-
-  return dayjs(utcTime).tz(localTimeZone).format('YYYY-MM-DD 00:00:00')
+  return dayjs.utc(utcTime).tz(localTimeZone).format('YYYY-MM-DD HH:mm:ss')
 }
 
 export const getTomorrowDate = () => {
@@ -14,5 +16,10 @@ export const getTomorrowDate = () => {
   dayjs.tz.setDefault(localTimeZone);
   const now = dayjs()
 
-  return now.add(1, 'd').format('YYYY-MM-DD 00:00:00')
+  return now.add(1, 'd').format('YYYY-MM-DD 02:00:00')
+}
+
+export const getDay = (date: string) => {
+  const dayIndex = dayjs(date).day()
+  return dayArray[dayIndex]
 }
